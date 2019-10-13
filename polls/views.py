66f956001e2469df456f3809a7571fa2ce5a2f1b
Fakhruddin90 <dinmsa@gmail.com>
2019-10-13 23:23:54 +0800

    polls commit

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
+# from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
 
+from .models import Question
 
 def index(request):
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    # return render(request, 'polls/index.html', context)
+    #use with HttpResponse
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
     return HttpResponse("Hello, world. You're at the polls index.")
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
